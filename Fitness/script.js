28dc5320db9595213267afc1343b089ff1886d06
HiruMihai <hirumihai@yahoo.com>
2024-05-23 01:41:45 +0300

    Revert "compressed video and images"
    This reverts commit f0cc0e634c7766ad5b6174a83dc5d80bbacf4b4d.

--- Fitness/script.js
+++ Fitness/script.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
             let elementOffset = $(this).offset().top;
             let distanceFromBottom = elementOffset - scrollTop - windowHeight;
 
-            let delay = 150;
+            let delay = 200;
             if (distanceFromBottom < -delay && !$(this).hasClass('animate__animated')) {
                 $(this).addClass('animate__animated animate__bounceInLeft');
                 $(this).css('opacity', '1');
