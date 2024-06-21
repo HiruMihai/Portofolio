1e37b946fd9890268b6f6f97df8b0fc41761c6a6
HiruMihai <hirumihai@yahoo.com>
2024-06-21 19:26:36 +0300

    .

--- home.js
+++ home.js
@@ -6,7 +6,6 @@ document.addEventListener("DOMContentLoaded", function() {
         const expandMoreIcon = btn.querySelector('.material-symbols-outlined:nth-child(2)');
         const expandLessIcon = btn.querySelector('.material-symbols-outlined:nth-child(3)');
 
-        // Set initial display of icons based on paragraph visibility
         if (paragraph.style.display === "block") {
             expandMoreIcon.style.display = "none";
             expandLessIcon.style.display = "inline";
@@ -18,35 +17,33 @@ document.addEventListener("DOMContentLoaded", function() {
         btn.addEventListener("click", function() {
             const projectBtns = this.parentElement.querySelectorAll(".project-btn");
 
-            // Toggle the display of the paragraph with opacity change
             if (paragraph.style.display === "none" || paragraph.style.display === "") {
                 paragraph.style.display = "block";
                 expandMoreIcon.style.display = "none";
                 expandLessIcon.style.display = "inline";
                 setTimeout(() => {
                     paragraph.style.opacity = "1";
-                }, 10); // Delay added to ensure transition starts after display change
+                }, 10);
             } else {
                 paragraph.style.opacity = "0";
                 expandMoreIcon.style.display = "inline";
                 expandLessIcon.style.display = "none";
                 setTimeout(() => {
                     paragraph.style.display = "none";
-                }, 300); // Matching the transition duration
+                }, 300);
             }
-            
-            // Toggle the display of project buttons with opacity change
+          
             projectBtns.forEach(btn => {
                 if (btn.style.display === "none" || btn.style.display === "") {
                     btn.style.display = "block";
                     setTimeout(() => {
                         btn.style.opacity = "1";
-                    }, 10); // Delay added to ensure transition starts after display change
+                    }, 10);
                 } else {
                     btn.style.opacity = "0";
                     setTimeout(() => {
                         btn.style.display = "none";
-                    }, 300); // Matching the transition duration
+                    }, 300);
                 }
             });
         });
