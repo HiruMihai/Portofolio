375e3cdcef6d0b9e61081010ac21db3ee963d752
HiruMihai <hirumihai@yahoo.com>
2026-07-28 19:10:57 +0300

    .

--- djheroo.com/script.js
+++ djheroo.com/script.js
@@ -1,15 +1,15 @@
 // Listen for when the playlists menu starts to open
-document
-  .getElementById("playlistsMenu")
-  .addEventListener("show.bs.collapse", function () {
-    // Smoothly scroll to the absolute bottom of the page
-    setTimeout(() => {
-      window.scrollTo({
-        top: document.body.scrollHeight,
-        behavior: "smooth",
-      });
-    }, 100); // A tiny 100ms delay ensures the content is rendered before calculating the final page height
-  });
+// document
+//   .getElementById("playlistsMenu")
+//   .addEventListener("show.bs.collapse", function () {
+//     // Smoothly scroll to the absolute bottom of the page
+//     setTimeout(() => {
+//       window.scrollTo({
+//         top: document.body.scrollHeight,
+//         behavior: "smooth",
+//       });
+//     }, 100); // A tiny 100ms delay ensures the content is rendered before calculating the final page height
+//   });
 
 // Wait for entrance animations to finish, then delete them
 document.querySelectorAll(".item").forEach((item) => {
